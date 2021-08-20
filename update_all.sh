566c9863f62a6e894df0d9bab6ea170b08b3d3a9
Nathan Glover <nathan@glovers.id.au>
2021-08-20 12:44:23 +0800

    added g4 update

--- update_all.sh
+++ update_all.sh
@@ -6,6 +6,7 @@
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF4.git f4
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF7.git f7
 ./update.sh git@github.com:STMicroelectronics/STM32CubeG0.git g0
+./update.sh git@github.com:STMicroelectronics/STM32CubeG4.git g4
 ./update.sh git@github.com:STMicroelectronics/STM32CubeH7.git h7
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL0.git l0
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL1.git l1
